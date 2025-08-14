d114433e772d8bf2e8a4375f3f836e94be12f1a6
smaheshwari1 <Sachin_Maheshwari@intuit.com>
2025-08-14 14:04:48 +0530

    Adding whole .config folder.

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    source /Users/smaheshwari1/.aliases
+end
+
+
+# Added by Windsurf
+fish_add_path /Users/smaheshwari1/.codeium/windsurf/bin
---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim", "folke/tokyonight.nvim", "catppuccin/nvim", "akinsho/bufferline.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = { style = "moon" },
+  },
+}
